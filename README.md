66bc4463683b494d240d5a14f8dd9d9ffe35d506
Sakhawat Hossain <sakhwt.hssain@gmail.com>
2023-04-08 17:28:51 +0600

    Upload

---
+++ README.md
@@ -0,0 +1,54 @@
+# GitGittu
+
+GitGittu is a simple tool that allows users to view their GitHub repositories, gists, followers, and following. The tool is built with JavaScript and utilizes the GitHub REST API.
+
+## Usage
+
+To use the tool, simply visit the [GitGittu website](https://shrudra.github.io/gitgittu/) and enter your GitHub username in the input field. Then click on the button for the information you want to view: Repositories, Gists, Followers, or Following. The information will be displayed in a table format below the button.
+
+## Screenshots
+
+### Repositories
+![Repositories](screenshots/repositories.png)
+
+### Gists
+![Gists](screenshots/gists.png)
+
+### Followers
+![Followers](screenshots/followers.png)
+
+### Following
+![Following](screenshots/following.png)
+
+## Development
+
+If you want to make modifications to GitGittu, you can clone the GitHub repository and make changes locally. Here are the steps to do so:
+
+1. Clone the repository:
+
+    ```
+    git clone https://github.com/shrudra/gitgittu.git
+    ```
+
+2. Open the index.html file in your browser to view the current version of the tool.
+
+3. Make any changes you want to the JavaScript code in the script.js file.
+
+4. Test your changes by refreshing the index.html file in your browser.
+
+5. Once you're satisfied with your changes, commit and push them to the GitHub repository:
+
+    ```
+    git add .
+    git commit -m "Description of changes made"
+    git push
+    ```
+
+## Contribute
+
+If you find any bugs or want to contribute to the project, feel free to submit an issue or pull request.
+
+## Contact
+
+If you have any questions or want to reach out, you can email me at [sakhwt.hssain@gmail.com](mailto:sakhwt.hssain@gmail.com).
+
---
+++ main.js
@@ -0,0 +1,54 @@
+const fetchData = (username, endpoint, columns) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://api.github.com/users/${username}/${endpoint}`, true);
+  
+  request.onload = () => {
+    const data = JSON.parse(request.response);
+    
+    if (request.status >= 200 && request.status < 400) {
+      let statusHTML = '';
+      statusHTML += '<tr>';
+      columns.forEach(column => {
+        statusHTML += `<th>${column}</th>`;
+      });
+      statusHTML += '</tr>';
+      data.forEach(status => {
+        statusHTML += '<tr>';
+        columns.forEach(column => {
+          if (column === 'html_url') {
+            statusHTML += `<td><a href="${status[column]}">${status[column]}</a></td>`;
+          } else if (column === 'avatar_url') {
+            statusHTML += `<td><img src="${status.avatar_url}" width="150" height="150"></td>`;
+          } else {
+            statusHTML += `<td>${status[column]}</td>`;
+          }
+        });
+        statusHTML += '</tr>';
+      });
+      document.querySelector('tbody').innerHTML = statusHTML;
+    } else {
+      document.querySelector('tbody').innerHTML = '<tr><td class="text-center">User not found or an error occurred</td></tr>';
+    }
+  }
+  request.send();
+}
+
+document.querySelector('#submit-btn').addEventListener('click', () => {
+  const username = document.querySelector('#username').value.trim();
+  fetchData(username, 'repos', ['full_name', 'language', 'html_url']);
+});
+
+document.querySelector('#submit-btn2').addEventListener('click', () => {
+  const username = document.querySelector('#username').value.trim();
+  fetchData(username, 'gists', ['filename', 'html_url', 'created_at']);
+});
+
+document.querySelector('#submit-btn3').addEventListener('click', () => {
+  const username = document.querySelector('#username').value.trim();
+  fetchData(username, 'following', ['avatar_url', 'login', 'html_url']);
+});
+
+document.querySelector('#submit-btn4').addEventListener('click', () => {
+  const username = document.querySelector('#username').value.trim();
+  fetchData(username, 'followers', ['avatar_url', 'login', 'html_url']);
+});
